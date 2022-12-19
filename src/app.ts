35ae10aecad6b20ff3aa8db3c7af9ed4667789ec
Daniel Lord-Doyle <dlorddoyle@vhslab.com>
2022-12-19 18:45:23 +1100

    added firebase

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuvGpAx5mBATg9XLZ-9_6zGBytyCldcMI",
+  authDomain: "fir-practice-700fc.firebaseapp.com",
+  projectId: "fir-practice-700fc",
+  storageBucket: "fir-practice-700fc.appspot.com",
+  messagingSenderId: "605343374344",
+  appId: "1:605343374344:web:3ecf7700fc5f2caa85c5bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+import { Number, String } from "runtypes"
+
+export interface Config {
+  name: String;
+  port: Number;
+  paginationLimit: Number;
+  
+}
