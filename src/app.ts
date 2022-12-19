9bec63532d92bc13fb116cdfa4933b71af495aa9
Daniel Lord-Doyle <dlorddoyle@vhslab.com>
2022-12-19 18:55:27 +1100

    testing rules

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +16,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+const testFunction = async () => {
+  const querySnapshot = await getDocs(collection(db, 'cafes'));
+  
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id)
+  })
+}
+
+testFunction()
